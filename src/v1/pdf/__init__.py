a82a4f05ddd62653726854350185ce6fc679df4e
João Victor <joaovictorslf@gmail.com>
2020-10-21 20:02:02 -0300

    File write added

--- src/v1/pdf/__init__.py
+++ src/v1/pdf/__init__.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, Flask
 from flask_restful import Api
 from .resources.merge import PdfMerger
+from .resources.write import PdfWriter
 
 bp = Blueprint("api", __name__, url_prefix="/api/v1")
 api = Api(bp)
 
 
-def init_app(app:Flask):
+def init_app(app: Flask):
     api.add_resource(PdfMerger, "/pdf-merge")
+    api.add_resource(PdfWriter, "/pdf-write")
     app.register_blueprint(bp)
