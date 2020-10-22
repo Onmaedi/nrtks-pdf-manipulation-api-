93ea690fadd2ae4bf7d3ee88cb1ffc7c53427ba8
João Victor <joaovictorslf@gmail.com>
2020-10-22 11:42:22 -0300

    change on Procfile

--- src/app.py
+++ src/app.py
@@ -10,13 +10,7 @@ def minimal_app():
 
 
 def create_app():
-    print("APP INITIALIZED")
     app = minimal_app()
     FlaskDynaconf(app)
     app.config.load_extensions()
     return app
-
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0")
