f3e13bb689c04b61784b7a17c87415d8b2a47baf
João Victor <joaovictorslf@gmail.com>
2020-10-23 09:51:52 -0300

    Folder merger added

--- src/v1/pdf/__init__.py
+++ src/v1/pdf/__init__.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from .resources.merge import PdfMerger
 from .resources.write import PdfWriter
 from .resources.merge_dropbox import PdfMergerDropbox
+from .resources.merge_dropbox_folder import MergeDropboxFolder
 
 bp = Blueprint("api", __name__, url_prefix="/api/v1")
 api = Api(bp)
@@ -12,4 +13,5 @@ def init_app(app: Flask):
     api.add_resource(PdfMergerDropbox, "/pdf-merge-dropbox")
     api.add_resource(PdfMerger, "/pdf-merge")
     api.add_resource(PdfWriter, "/pdf-write")
+    api.add_resource(MergeDropboxFolder, "/pdf-merge-folder")
     app.register_blueprint(bp)
---
+++ src/v1/pdf/resources/merge_dropbox_folder.py
@@ -0,0 +1,87 @@
+import os
+from os import path
+import json
+import base64
+from uuid import uuid4
+
+from flask import request, jsonify, abort
+from flask_restful import Resource
+import dropbox
+from dropbox.files import ListFolderResult, FileMetadata
+import PyPDF2
+
+
+class DropboxFileHandler:
+    def __init__(self):
+        self.__token = os.getenv("DROPBOX_TOKEN")
+        self.__dropbox = dropbox.Dropbox(self.__token)
+
+    def download(self, dropbox_file_path, tmp_folder_path):
+        try:
+            *_, extension = path.splitext(dropbox_file_path)
+            tmp_file_name = f"{str(uuid4())}{extension}"
+            tmp_file_path = path.join(tmp_folder_path, tmp_file_name)
+
+            *_, response = self.__dropbox.files_download(
+                path=dropbox_file_path)
+
+            with open(tmp_file_path, "wb+") as temp_file:
+                temp_file.write(response.content)
+
+            return tmp_file_path
+        except:
+            ...
+
+    def folder_list_files(self, folder_path: str) -> ListFolderResult:
+        files = self.__dropbox.files_list_folder(folder_path)
+        return files
+
+
+class MergeDropboxFolder(Resource):
+    def __init__(self):
+        ...
+
+    def merger(self, tmp_folder_path, files_path: list = []) -> str:
+        merger = PyPDF2.PdfFileMerger()
+
+        output_path = path.join(f"{tmp_folder_path}", f"{str(uuid4())}.pdf")
+        for file in files_path:
+            merger.append(file)
+
+        merger.write(output_path)
+        return output_path
+
+    def post(self):
+        try:
+            tmp_folder_name = str(uuid4())
+            tmp_folder_path = path.join(path.abspath(""), tmp_folder_name)
+            os.makedirs(tmp_folder_path)
+            tmp_files = []
+            merged_files = []
+
+            request_body = json.loads(request.data)
+            folder_path = request_body["folderPath"]
+            files = DropboxFileHandler().folder_list_files(folder_path)
+            for file in files.entries:
+                tmp_files.append(DropboxFileHandler().download(
+                    file.path_display, tmp_folder_path))
+                merged_files.append(file.path_display)
+
+            tmp_merged_file_path = self.merger(tmp_folder_path, tmp_files)
+
+            tmp_files.append(tmp_merged_file_path)
+
+            with open(tmp_merged_file_path, "rb") as merged:
+                merged_pdf_base64 = str(
+                    base64.b64encode(merged.read()), 'utf-8')
+
+            for tmp_file in tmp_files:
+                os.remove(tmp_file)
+            os.rmdir(tmp_folder_path)
+
+            return jsonify({
+                "filesMerged": merged_files,
+                "base64file": merged_pdf_base64
+            })
+        except:
+            abort(500)
